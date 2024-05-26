7a84ec9c4f8f9ef49db8bb5e907a31de2c4f7ee9
Arpit319-byte <arpitchauhanupal@gmail.com>
2024-05-26 16:16:25 +0530

    UserRepository extends JpaRepository interface

---
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.Quora_Backend_Application.repository;
+
+import com.example.Quora_Backend_Application.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+interface UserRepository extends JpaRepository<User, UUID> {
+
+}
