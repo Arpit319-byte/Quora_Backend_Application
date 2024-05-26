b3553189711c11460696384fece3de8d10f57b6a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-05-26 19:45:42 +0530

    Removed the unnecessary code from the UserAction

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/UserAction.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/UserAction.java
@@ -1,7 +1,6 @@
 package com.example.Quora_Backend_Application.controller;
 
 import com.example.Quora_Backend_Application.models.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
