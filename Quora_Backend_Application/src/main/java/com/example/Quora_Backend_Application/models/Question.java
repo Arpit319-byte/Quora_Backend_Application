e366b2f4f72f7b25fdbce918ab115002b3133065
Arpit319-byte <arpitchauhanupal@gmail.com>
2024-05-26 11:37:02 +0530

    Added data members for Question class

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/models/Question.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/models/Question.java
@@ -1,4 +1,28 @@
 package com.example.Quora_Backend_Application.models;
 
-public class Question {
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question extends BaseClass {
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private List<UUID> topics;
+
+    @OneToOne
+    private User user;
 }
