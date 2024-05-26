a96d8bc7125818f8c66c40ffec7ea2907b42f13a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-05-26 19:44:44 +0530

    Made changes to Comment

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/models/Comment.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/models/Comment.java
@@ -1,6 +1,5 @@
 package com.example.Quora_Backend_Application.models;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
