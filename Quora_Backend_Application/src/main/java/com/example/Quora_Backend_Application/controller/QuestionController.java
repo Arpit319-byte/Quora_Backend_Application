7202acd9c04806cf15392d83abe1e1bed2942d6f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-05 15:35:22 +0530

    Added Question service class

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/QuestionController.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/QuestionController.java
@@ -1,4 +1,18 @@
 package com.example.Quora_Backend_Application.controller;
 
+import com.example.Quora_Backend_Application.models.Question;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/question")
 public class QuestionController {
+
+    //@GetMapping("/{questionId}")
+
+    @PostMapping
+    public ResponseEntity<String> creteQuestion(@RequestBody Question question){
+
+    }
 }
