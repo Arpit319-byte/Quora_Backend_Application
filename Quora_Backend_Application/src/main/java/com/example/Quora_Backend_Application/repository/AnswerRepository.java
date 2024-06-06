da9ca8f82360e62ce7740a85dc29f160514932a3
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-06 23:17:10 +0530

    Added Answer service class

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/repository/AnswerRepository.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/repository/AnswerRepository.java
@@ -1,4 +1,9 @@
 package com.example.Quora_Backend_Application.repository;
 
-public interface AnswerRepository {
+import com.example.Quora_Backend_Application.models.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer,Long> {
 }
