a7f93b981832457ed90d18fcaf3642705ed0bc9a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-07 13:02:31 +0530

    Added the Get and Post method in the topic service class

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/TopicController.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/TopicController.java
@@ -1,10 +1,35 @@
 package com.example.Quora_Backend_Application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Quora_Backend_Application.models.Topic;
+import com.example.Quora_Backend_Application.services.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/topics")
 public class TopicController {
+
+    private final TopicService topicService;
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Topic>>getAllTopics(){
+        return ResponseEntity.ok(topicService.getAllTopics());
+    }
+
+    @PostMapping
+    public ResponseEntity<Topic> createTopic(@RequestBody  Topic topic){
+        Topic topic1 = topicService.createTopic(topic);
+        return new ResponseEntity<>(topic, HttpStatus.CREATED);
+    }
 
 }
--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/services/TopicService.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/services/TopicService.java
@@ -1,2 +1,29 @@
-package com.example.Quora_Backend_Application.services;public class TopicService {
+package com.example.Quora_Backend_Application.services;
+
+import com.example.Quora_Backend_Application.models.Topic;
+import com.example.Quora_Backend_Application.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicService{
+
+    private final TopicRepository topicRepository;
+
+    @Autowired
+    public TopicService(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+
+    public Topic createTopic(Topic topic) {
+        topicRepository.save(topic);
+        return topic;
+    }
 }
