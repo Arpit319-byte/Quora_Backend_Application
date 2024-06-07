e001d56a6189c6965f2e62c8ab73520fe151cded
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-07 12:41:42 +0530

    Added the Topic Repository class

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/repository/TopicRepository.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/repository/TopicRepository.java
@@ -1,7 +1,9 @@
 package com.example.Quora_Backend_Application.repository;
 
+import com.example.Quora_Backend_Application.models.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TopicRepository {
+public interface TopicRepository extends JpaRepository<Topic,Long> {
 }
