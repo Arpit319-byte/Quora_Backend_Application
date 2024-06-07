c60a7429e3af23d54793bde4630517db2c41ef98
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-07 11:45:16 +0530

    Added Get,Post,Put,Delete mapping in Answer Controller

--- Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/AnswerController.java
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/AnswerController.java
@@ -1,8 +1,53 @@
 package com.example.Quora_Backend_Application.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Quora_Backend_Application.models.Answer;
+import com.example.Quora_Backend_Application.services.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    @Autowired
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @GetMapping("/answer/{questionId}")
+    public ResponseEntity<List<Answer>> getAllAnswers(@PathVariable Long questionId){
+      List<Answer> answers=answerService.getAllAnswers(questionId);
+        return new ResponseEntity<>(answers, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/question/{questionId}/answer")
+    public ResponseEntity<Answer> createAnswer(@RequestBody Answer answer){
+        Answer answer1 = answerService.createAnswer(answer);
+        return new ResponseEntity<>(answer1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/answer/{answerId}")
+    public ResponseEntity<Answer> updateAnswer(@PathVariable Long answerId, @RequestBody Answer answer){
+        Answer answer1 = answerService.updateAnswer(answerId, answer);
+        return new ResponseEntity<>(answer1, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/answer/{answerId}")
+    public ResponseEntity<String> deleteAnswer(@PathVariable Long answerId){
+
+        Answer answer= answerService.getAnswerById(answerId);
+        if(answer!=null) {
+            answerService.deleteAnswerById(answerId);
+            return new ResponseEntity<>("Answer Deleted Successfully", HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>("Answer Not Found", HttpStatus.NOT_FOUND);
+    }
 
 }
