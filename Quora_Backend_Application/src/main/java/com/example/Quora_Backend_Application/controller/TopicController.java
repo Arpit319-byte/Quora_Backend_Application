2dad01968a289b3aec5bbd0b522b868e3bbc233f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-06-07 12:34:38 +0530

    Added the Topic Controller class

---
+++ Quora_Backend_Application/src/main/java/com/example/Quora_Backend_Application/controller/TopicController.java
@@ -0,0 +1,10 @@
+package com.example.Quora_Backend_Application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topic")
+public class TopicController {
+
+}
